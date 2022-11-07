84c3a8ce7bc9e0544acea78d15e6806a2de0faa6
Felipe <fquiceno1@misena.edu.co>
2022-11-07 17:33:37 -0500

    [CREATE], Creacion de la interface userService y la implementacion con la logica para guardado de usuario

--- sistema-examenes-backend/src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
+++ sistema-examenes-backend/src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
@@ -1,13 +1,18 @@
 package com.sistema.examenes;
 
+import com.sistema.examenes.entities.User;
+import com.sistema.examenes.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaExamenesBackendApplication {
+public class SistemaExamenesBackendApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
 	}
+
 
 }
--- sistema-examenes-backend/src/main/java/com/sistema/examenes/repositories/UserRepository.java
+++ sistema-examenes-backend/src/main/java/com/sistema/examenes/repositories/UserRepository.java
@@ -5,5 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    /**
+     * findByUsername utilizando JpaRepository, crea una implementacion
+     * automaticamente usando el estadar con el que se nombra este metodo
+     * @param username Usuario que desea guardar
+     * @return  Usuario guardado
+     */
     public User findByUsername(String username);
 }
