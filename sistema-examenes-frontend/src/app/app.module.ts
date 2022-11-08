bca4e270554fee061ea6745541ab4854723b2560
Felipe <fquiceno1@misena.edu.co>
2022-11-08 07:33:27 -0500

    [CREATE],Se crea el formulario, se importan 3 modulos de AngularMaterial

--- sistema-examenes-frontend/src/app/app.module.ts
+++ sistema-examenes-frontend/src/app/app.module.ts
@@ -8,6 +8,8 @@ import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { LoginComponent } from './pages/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
