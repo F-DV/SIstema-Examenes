5439f8189fc86e22852b28cc41302b326d014216
Felipe <fquiceno1@misena.edu.co>
2022-11-09 13:18:19 -0500

    [CREATE],Creacion de paquete y clases para la seguridad con JWT

--- sistema-examenes-backend/src/main/java/com/sistema/examenes/entities/User.java
+++ sistema-examenes-backend/src/main/java/com/sistema/examenes/entities/User.java
@@ -1,8 +1,12 @@
 package com.sistema.examenes.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sistema.examenes.security.Authority;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,7 +15,7 @@ import java.util.Set;
  */
 @Entity
 @Table(name = "usuarios")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto-incremen cuando cree nuevo registro
@@ -48,6 +52,42 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    /**
+     * Recorremos los roles que tiene el usuario en cuestion,
+     * obtenemos los nombres de los roles
+     * los guardamos en una lista de autoridades
+     * @return la lista con las autoridades del usuario
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Authority> autorities = new HashSet<>();
+
+        this.userRoles.forEach(userRole -> {
+            autorities.add(new Authority(userRole.getRole().getNameRole()));
+        });
+        return autorities;
     }
 
     public void setUsername(String username) {
