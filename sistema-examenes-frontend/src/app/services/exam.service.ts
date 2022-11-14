dc184fc2e765a3adf0956ac198235131b10ff96d
Felipe <fquiceno1@misena.edu.co>
2022-11-13 23:17:13 -0500

    [CREATE],Vista de examenes y servicio de examenes

---
+++ sistema-examenes-frontend/src/app/services/exam.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExamService {
+
+  constructor(private http:HttpClient) { }
+
+  public listExams(){
+    return this.http.get(`${baseUrl}examen/`);
+  }
+}
